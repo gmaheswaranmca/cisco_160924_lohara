4bc8937ace985448d59dfc1f22199784b56465df
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-09-16 12:35:25 +0530

    Intro to py

---
+++ 2024091/Employee.py
@@ -0,0 +1,13 @@
+class Employee:
+    def __init__(self, name, address, code, salary):
+        self.name = name
+        self.address = address
+        self.code = code
+        self.salary = salary
+
+    def __str__(self):
+        return f'{self.name}, {self.address}, {self.code}, {self.salary}'
+
+    
+mahesh = Employee('Maheswaran', '2/12, alagu nagar', 'PYABC10021', 200000)
+print(mahesh)
---
+++ 2024091/sum.py
@@ -0,0 +1,5 @@
+def find_sum(first, second):
+    return first + second 
+
+s1 = find_sum(10,20)
+print(s1)
