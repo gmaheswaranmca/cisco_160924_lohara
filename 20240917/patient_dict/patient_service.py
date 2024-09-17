69e59d3226b329c474ae94af4cc6e2da6339b555
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-09-17 11:46:08 +0530

    Intro to py

--- 20240917/patient_dict/patient_service.py
+++ 20240917/patient_dict/patient_service.py
@@ -1,54 +1,47 @@
 from Patient import Patient 
 #patient = Patient(100,'Rohit')
 #print(patient)
-
-#2. patients[]
-patients = []
-
+#2. patients{}
+patients = {} #dict()
 #3. patient_add(id, name)
 def patient_add(id, name):
     global patients
     patient = Patient(id, name)
-    patients.append(patient)
+    patients[patient.id] = patient #patients.append(patient)
     print('Patient created successfully')
 #4. patient_remove(id)
 def patient_remove(id):
     global patients
-    for patient in patients:
-        if patient.id == id:
-            print(patient)
-            if input('Are you sure to delete(yes/no)?').lower() == 'yes':
-                patients.remove(patient)
-                print('Patient deleted successfully')
-            return 
-        #end if 
-    #end for 
-    print(f'No such id {id}')
+    patient = patients.get(id)
+    if patient == None:
+        print(f'No such id {id}')
+        return 
+    print(patient)
+    if input('Are you sure to delete(yes/no)?').lower() == 'yes':
+        del patients[id] #patients.remove(patient)
+        print('Patient deleted successfully')
+    #end if     
 #5. patient_display()
 def patient_display():
     global patients
-    for patient in patients:
-        print(patient)
+    for id in patients: #for patient in patients: 
+        print(patients[id]) #print(patient)
 #patient_display_byid(id)
 def patient_display_byid(id):
     global patients
-    for patient in patients:
-        if patient.id == id:
-            print(patient)
-            return 
-        #endif
-    #endfor 
-    print('Invalid id')
+    patient = patients.get(id)
+    if patient == None:
+        print(f'No such id {id}')
+        return 
+    print(patient)    
 #patient_update(id)
 def patient_update(id):
     global patients
-    for patient in patients:
-        if patient.id == id:
-            print(patient)
-            name = input(f'Enter new name({patient.name}):')
-            patient.name = name 
-            print('Patient updated successfully')
-            return 
-        #end if 
-    #end for 
-    print(f'No such id {id}')
+    patient = patients.get(id)
+    if patient == None:
+        print(f'No such id {id}')
+        return 
+    print(patient)
+    name = input(f'Enter new name({patient.name}):')
+    patient.name = name 
+    print('Patient updated successfully')
