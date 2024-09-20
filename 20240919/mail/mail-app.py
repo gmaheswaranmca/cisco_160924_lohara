44cc86a817393fcdb3b8b3b31a959676c3efeee8
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-09-20 09:46:10 +0530

    Intro to py

--- 20240919/mail/mail-app.py
+++ 20240919/mail/mail-app.py
@@ -10,15 +10,26 @@ Then
     #https://support.google.com/accounts/answer/185833?hl=en
 '''
 
-
+import subprocess
+serial = subprocess.check_output('wmic bios get serialnumber').decode("utf-8").replace('SerialNumber','').strip() 
 
 import smtplib as smtp
-
+from datetime import datetime
 connection = smtp.SMTP_SSL('smtp.gmail.com', 465)
     
 email_addr = 'pystud19@gmail.com'
 email_passwd = 'yaxb hoco fihc prnn'
 connection.login(email_addr, email_passwd)
-connection.sendmail(from_addr=email_addr, to_addrs='gmaheswaranmca@gmail.com', msg="Sent from my IDE. Hehe")
+
+receiver='gmaheswaranmca@gmail.com'
+mail_body="I develop app to send mails"
+dt_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+subject = f'my app {dt_time} @{serial}'
+
+from email.mime.multipart import MIMEMultipart
+msg = MIMEMultipart()
+msg['From'] = email_addr
+msg['To'] = receiver
+msg['Subject'] = subject
+connection.sendmail(email_addr, receiver, msg.as_string())
 connection.close()
-print('Mail sent successfully')
