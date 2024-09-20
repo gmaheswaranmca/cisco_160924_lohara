2889f87a8e7028c4607a3a77e843a4a70fca8f0c
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-09-20 09:11:30 +0530

    Intro to py

--- 20240919/mail/mail-app.py
+++ 20240919/mail/mail-app.py
@@ -1,5 +1,17 @@
 #https://stackoverflow.com/questions/72480454/sending-email-with-python-google-disables-less-secure-apps
 
+'''
+To send mail 
+    enable in gmail the 2 step verification 
+Then 
+    use below link to create app password
+    with app password and your mail id try to run the below code
+
+    #https://support.google.com/accounts/answer/185833?hl=en
+'''
+
+
+
 import smtplib as smtp
 
 connection = smtp.SMTP_SSL('smtp.gmail.com', 465)
@@ -9,3 +21,4 @@ email_passwd = 'yaxb hoco fihc prnn'
 connection.login(email_addr, email_passwd)
 connection.sendmail(from_addr=email_addr, to_addrs='gmaheswaranmca@gmail.com', msg="Sent from my IDE. Hehe")
 connection.close()
+print('Mail sent successfully')
